a16db72a1e285be2f212763ebf60051f94e3cec4
lee_jjoon <whitour@gmail.com>
2020-09-12 07:58:20 +0900

    feat: 필요없는 기본 파일 및 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  flex: 1;
+  background: #fff;
+	display: flex;
+	width: 100%
+	height: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Lets gogogo</Title>
+    </Container>
   );
 }
 
