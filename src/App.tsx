8e6d0ada3ba8bfd4fcd3478b0362e3398ae0201c
lee_jjoon <whitour@gmail.com>
2020-09-12 09:25:21 +0900

    feat: App.tsx Provider 및 TodoList attach

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import TodoProvider from "src/stores/context/TodoProvider";
+import ContextTodoList from "src/ContextTodoList";
 
 const Container = styled.div`
   flex: 1;
   background: #fff;
 	display: flex;
+	flex-direction: column;
 	width: 100%
 	height: 100%;
 `;
@@ -12,12 +15,16 @@ const Container = styled.div`
 const Title = styled.div`
   font-size: 20px;
   font-weight: bold;
+  margin-bottom: 32px;
 `;
 function App() {
   return (
-    <Container>
-      <Title>Lets gogogo</Title>
-    </Container>
+    <TodoProvider>
+      <Container>
+        <Title>Lets gogogo</Title>
+        <ContextTodoList />
+      </Container>
+    </TodoProvider>
   );
 }
 
